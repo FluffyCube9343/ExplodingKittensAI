64099dc72ed4a95e7baa6c9ac8ba328bdc6f2358
fluffycube9343 <learningninja007@gmail.com>
2025-07-15 13:11:51 -0400

    mongodb

--- main.py
+++ main.py
@@ -1,6 +1,22 @@
 import random
 from players import *
 import time; ctic = time.time()
+from pymongo import MongoClient, InsertOne
+
+
+# 1. Connect to MongoDB
+
+
+
+client = MongoClient("mongodb://localhost:27017", w=1, journal=True)
+
+
+db = client["ekgame"]
+
+
+states = db["states"]
+
+favors = db["favors"]
 
 #Deck Structure
 
@@ -29,6 +45,14 @@ def initDeck(deck, playerdecks, players, PLAYERS):
 
 # while len(players):
 
+def log_state(state, attr, table):
+    pass
+    state_str = str(state)
+    table.update_one(
+        {"_id": state_str},
+        {"$inc": {attr: 1}},
+        upsert = True
+    )
 
 def simulateGame(PLAYERS):
     deck = [1, 1, 1, 1, 1, 2, 2, 2, 2, 3, 3, 3, 3, 4, 4, 4, 4, 5, 5, 5, 5, 6, 6, 6, 6, 6, 7, 7, 7, 7, 8, 8, 8, 8, 9, 9, 9, 9, 10, 10, 10, 10, 11, 11, 11, 11]
@@ -42,11 +66,27 @@ def simulateGame(PLAYERS):
     toDraw = 1
     p1stf = [-9,-9,-9]
     p2stf = [-9,-9,-9]
+
+    p1states = []
+    p2states = []
+
+    p1favors = []
+    p2favors = []
+
+
     while toDraw and len(players)>1:
         move = 'skibidi'
         while move:
             move = players[turn].getMove(toDraw, movectr, turnctr, [players[0].numCards, players[1].numCards])
             
+            if(not move): move = 0
+
+            if(turn==0):
+                p1states.append([len(deck),toDraw]+p1stf+players[0].hand+[len(players[1].hand),move])
+            
+            if(turn==1):
+                p2states.append([len(deck),toDraw]+p1stf+players[1].hand+[len(players[0].hand),move])
+
             if(move):
                 players[turn].numCards -= 1
                 playerdecks[turn][move] -= 1
@@ -68,6 +108,10 @@ def simulateGame(PLAYERS):
                 movectr += 1
             elif(move==4):
                 favorcard = players[victim].getFavored()
+                if(turn==1):
+                    p1favors.append([len(deck),toDraw]+p1stf+players[0].hand+[len(players[1].hand),favorcard])
+                if(turn==10):
+                    p2favors.append([len(deck),toDraw]+p1stf+players[1].hand+[len(players[0].hand),favorcard])
                 players[turn].hand[favorcard] += 1 
                 players[turn].numCards += 1
                 players[turn].inform(turn, move, {'victim': victim, 'cardtaken': favorcard})
@@ -103,13 +147,37 @@ def simulateGame(PLAYERS):
                     deck.insert(players[turn].reinsertEK(len(deck)),-1)
             toDraw -= 1
             if(toDraw == 0): turn += 1; turn %= PLAYERS; toDraw = 1; turnctr += 1
+
+    for state in p1states:
+        log_state(state, "total", states)
+    for state in p2states:
+        log_state(state, "total", states)
+    for favor in p1states:
+        log_state(favor, "total", favors)
+    for favor in p2states:
+        log_state(favor, "total", favors)
+
+    if(players[0].name==0):
+        for state in p1states:
+            log_state(state, "win", states)
+        for favor in p1favors:
+            log_state(favor, "win", favors)
+    elif(players[0].name==1):
+        for state in p2states:
+            log_state(state, "win", states)
+        for favor in p2favors:
+            log_state(favor, "win", favors)
+    else:
+        assert 1==2, "wtf"
+
+
     return players[0].name
 
 
 if __name__ == '__main__':
     onewin = 0
     zerowin = 0
-    for _ in range(int(1e4)):
+    for _ in range(int(1e2)):
         res = simulateGame(2)
         # print(res)
         if(res==1): onewin += 1
