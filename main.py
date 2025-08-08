b1be4a05fd5532de9cf407965b194ccc3468d6d2
fluffycube9343 <learningninja007@gmail.com>
2025-08-08 08:35:51 -0400

    code cleanup and speedup

--- main.py
+++ main.py
@@ -11,24 +11,8 @@ import time; ctic = time.time()
 #EXPL = -1
 
 def initDeck(deck, playerdecks, players, PLAYERS):
-    # deck.extend([1 for i in range(5)])
-    # deck.extend([2 for i in range(4)])
-    # deck.extend([3 for i in range(4)])
-    # deck.extend([4 for i in range(4)])
-    # deck.extend([5 for i in range(4)])
-    # deck.extend([6 for i in range(5)])
-
-    # deck.extend([7 for i in range(4)])
-    # deck.extend([8 for i in range(4)])
-    # deck.extend([9 for i in range(4)])
-    # deck.extend([10 for i in range(4)])
-    # deck.extend([11 for i in range(4)])
-
-    # deck = [1, 1, 1, 1, 1, 2, 2, 2, 2, 3, 3, 3, 3, 4, 4, 4, 4, 5, 5, 5, 5, 6, 6, 6, 6, 6, 7, 7, 7, 7, 8, 8, 8, 8, 9, 9, 9, 9, 10, 10, 10, 10, 11, 11, 11, 11]
-
     rng.shuffle(deck)
-    # deck = selfshuffle(deck)
-    # print(playerdecks)
+
     for player in playerdecks:
         for i in range(7):
             player[deck.pop()]+=1
@@ -42,49 +26,36 @@ def initDeck(deck, playerdecks, players, PLAYERS):
     players.append(Player(1,playerdecks[1]))
 
 
-# while len(players):
-
-
 def simulateGame(PLAYERS):
     deck = [1, 1, 1, 1, 1, 2, 2, 2, 2, 3, 3, 3, 3, 4, 4, 4, 4, 5, 5, 5, 5, 6, 6, 6, 6, 6, 7, 7, 7, 7, 8, 8, 8, 8, 9, 9, 9, 9, 10, 10, 10, 10, 11, 11, 11, 11]
     playerdecks = [[1]+[0]*11 for n in range(PLAYERS)] #0 = me, 1+ = AIs
     players = []
     initDeck(deck, playerdecks, players, PLAYERS)
     turn = 0
-    turnctr = 0 # Number of cards drawn from deck
-    movectr = 0 # Number of ATK + SKIP + Cards Drawn
     victim = 1
     toDraw = 1
     while toDraw and len(players)>1:
         move = 'skibidi'
         while move:
-            move = players[turn].getMove(toDraw, movectr, turnctr, [players[0].numCards, players[1].numCards])
-            
+            move = players[turn].getMove(toDraw, players[victim].numCards)
             if(move):
                 players[turn].numCards -= 1
                 playerdecks[turn][move] -= 1
                 if(move>=7):
                     players[turn].numCards -= 1
                     playerdecks[turn][move] -= 1
-            victim = turn^1
-            # print('turn', turn, playerdecks[0], playerdecks[1], 'np0', players[0].numPlayable, 'np1', players[1].numPlayable, 'lendeck', len(deck), 'move', move)
             
             if(not move): continue
-            # for player in players:
-            #     player.inform(turn, move, victim if move==4 or move>=7 else None)
-
+    
             if(move==2):
                 toDraw = toDraw+1 if toDraw==1 else toDraw+2
                 turn += 1; turn %= PLAYERS
-                turnctr += 1
-                movectr += 1
+                victim = turn^1
             elif(move==3):
                 turn += 1; turn %= PLAYERS
-                turnctr += 1
-                movectr += 1
+                victim = turn^1
             elif(move==4):
                 favorcard = players[victim].getFavored()
-                # print('favorcard', favorcard)
                 players[turn].hand[favorcard] += 1 
                 players[turn].numCards += 1
                 players[turn].inform(turn, move, {'victim': victim, 'cardtaken': favorcard})
@@ -94,18 +65,14 @@ def simulateGame(PLAYERS):
                 players[turn].inform(turn,6,deck[:-4:-1])
             elif(move>=7):
                 cardtaken = random.choices([0,1,2,3,4,5,6,7,8,9,10,11], weights=players[victim].hand, k=1)[0]
-                # print('cardtaken', cardtaken)
                 players[victim].hand[cardtaken] -= 1
                 players[victim].numCards -= 1
                 players[victim].inform(turn, move, {'victim': victim, 'cardtaken': cardtaken})
                 players[turn].hand[cardtaken] += 1
                 players[turn].numCards += 1
                 players[turn].inform(turn, move, {'victim': victim, 'cardtaken': cardtaken})
-        # print(deck)
         nextcard = deck.pop()
-        # print('nextcard', nextcard)
         safe = players[turn].cardDrawn(nextcard)
-        movectr += 1
         if(not safe): players.pop(turn); toDraw = 1
         else:
             if(safe==1): 
@@ -113,19 +80,17 @@ def simulateGame(PLAYERS):
                 else:
                     deck.insert(players[turn].reinsertEK(len(deck)),-1)
             toDraw -= 1
-            if(toDraw == 0): turn += 1; turn %= PLAYERS; toDraw = 1; turnctr += 1
+            if(toDraw == 0): 
+                turn += 1; turn %= PLAYERS; 
+                toDraw = 1; 
+                victim = turn^1
     return players[0].name
-
-# print(players[0].hand)
-# print("Player",players[0].name,"won by",players[0].hand.count('DEF'),'defuses')
-# print(players[0].getMove())
 
 if __name__ == '__main__':
     onewin = 0
     zerowin = 0
     for _ in range(int(1e4)):
         res = simulateGame(2)
-        # print(res)
         if(res==1): onewin += 1
         else: zerowin += 1
 
--- players.py
+++ players.py
@@ -2,10 +2,6 @@ import random
 import numpy as np
 rng = np.random.default_rng()
 
-# randomlist = [int(i) for i in open('random256.txt').readlines()]
-# startind = random.randint(0,len(randomlist)//10)
-
-
 
 class Player: #RandomPlayer
     def __init__(self, name, hand):
@@ -25,13 +21,11 @@ class Player: #RandomPlayer
             elif(cardtaken >= 7 and self.hand[cardtaken]%2==0): self.numPlayable += 1
 
         
-    def getMove(self, toDraw, movectr, turnctr, deckhandlens):
-        #Return None = draw ONE card
+    def getMove(self, toDraw, deckhandlens):
         chosenmove = giveRandomMove(self.hand,self.name,deckhandlens, self.numPlayable)
         if(chosenmove!=None): self.numPlayable -= 1
-        # print('moving!', self.name, chosenmove)
         return chosenmove
-    def cardDrawn(self,card): #THIS CANNOT BE OVERWRITTEN
+    def cardDrawn(self,card):
         if(card==-1):
             if(not self.hand[0]):
                 return 0
@@ -57,137 +51,24 @@ class Player: #RandomPlayer
         return togiveaway
     def reinsertEK(self, decklen):
         return int(random.random()*(decklen+1))
-        # return random.randint(0,decklen)
 
     
 
 
-# class ARPlayer(Player): #AlternateRandomPlayeri
-#     def getMove(self, toDraw, movectr, turnctr, deckhandlens):
-#     #Return None = draw ONE card
-#         global startind
-#         startind += 1; startind %= 100000
-#         # print(startind)
-#         psbls = whatcaniplay(self.hand,self.name,deckhandlens)
-#         if(not psbls):
-#             return None
-#         else:
-#             playcard = randomlist[startind]%2
-#             if(not playcard):
-#                 return None
-#             else:
-#                 return psbls[randomlist[startind]%len(psbls)]
-#         # chosenmove = random.choice(psbls)
-
-# class CommonSensePlayer(Player): #CommonSensePlayer
-#     def __init__(self, name, hand):
-#         self.name = name
-#         self.hand = hand
-#         self.movehistory = {}
-#     def inform(self, player, move, moveData):
-#         pass
-#     def getMove(self, toDraw, movectr, turnctr, deckhandlens):
-#         #Return None = draw ONE card
-#         if(movectr not in self.movehistory): self.movehistory[movectr] = set()
-
-#         psbls = whatcaniplay(self.hand,self.name,deckhandlens)+[None]
-#         chosenmove = random.choice(psbls)
-
-#         if('STF' in self.movehistory[movectr]):
-#             while(chosenmove == 'STF'): 
-#                 # print('stf', chosenmove, self.hand)
-#                 chosenmove = random.choice(psbls)
-#         if('SHUF' in self.movehistory[movectr]):
-#             while(chosenmove == 'SHUF'): chosenmove = random.choice(psbls)
-#         # print(self.name, movectr, turnctr, chosenmove, self.hand)
-#         self.movehistory[movectr].add(chosenmove)
-#         return chosenmove
-#     def getFavored(self):
-#         index = random.randrange(len(self.hand))
-#         pickable = [i for i in self.hand if i!='DEF' and i!='FVR' and i!='NOPE']
-#         if(not pickable):
-#             if('NOPE' in self.hand): 
-#                 self.hand.remove('NOPE')
-#                 return 'NOPE'
-#             if('FVR' in self.hand): 
-#                 self.hand.remove('FVR')
-#                 return 'FVR'
-#             self.hand.remove('DEF')
-#             return 'DEF'
-            
-            
-#         chosen = pickable.pop(random.randrange(len(pickable)))
-#         self.hand.remove(chosen)
-#         return chosen
-#         # return self.hand.pop(random.randrange(len(self.hand)))
-
-# class RunningPlayer(CommonSensePlayer):
-#     def getMove(self, toDraw, movectr, turnctr, deckhandlens):
-#         global startind
-#         # startind += 1; startind %= 100000
-#         # playcard = randomlist[startind]%2
-#         # if(not playcard): return None
-#         # if(turnctr<15): return None
-#         if(turnctr<35 and self.hand.count('DEF')): return None
-#         # if(turnctr<20): return None
-#         handset = set(self.hand)
-#         if(movectr not in self.movehistory): self.movehistory[movectr] = set()
-
-#         psbls = whatcaniplay(self.hand,self.name,deckhandlens)+[None]
-#         if('FVR' in psbls): return 'FVR'
-#         if('C1' in psbls): return 'C1'
-#         if('C2' in psbls): return 'C2'
-#         if('C3' in psbls): return 'C3'
-#         if('C4' in psbls): return 'C4'
-#         if('C5' in psbls): return 'C5'
-
-#         if(toDraw > 1 and 'ATK' in psbls): return 'ATK'
-#         if(toDraw>1 and 'SKIP' in psbls): return 'SKIP'
-#         if(movectr>40 and 'ATK' in psbls): return 'ATK'
-#         if(movectr>40 and 'SKIP' in psbls): return 'SKIP'
-#         # catcards = [i for i in psbls if type(i)==str and i[0]=='C']
-#         # if(catcards): return catcards[0]
-
-
-
-#         chosenmove = random.choice(psbls)
-
-#         if('STF' in self.movehistory[movectr]):
-#             while(chosenmove == 'STF'): 
-#                 # print('stf', chosenmove, self.hand)
-#                 chosenmove = random.choice(psbls)
-#         if('SHUF' in self.movehistory[movectr]):
-#             while(chosenmove == 'SHUF'): chosenmove = random.choice(psbls)
-#         # print(self.name, movectr, turnctr, chosenmove, self.hand)
-#         self.movehistory[movectr].add(chosenmove)
-#         return chosenmove
-    
-#     def reinsertEK(self, decklen):
-#         return 1
-
-
-def weighted_random_choice(choices, weights):
-    cum_weights = np.cumsum(weights)
-    total_weight = cum_weights[-1]
-    rand_val = np.random.rand() * total_weight
-    idx = np.searchsorted(cum_weights, rand_val)
-    return choices[idx]
 
 
     
 def giveRandomMove(deck,name,deckhandlens,numPlayable,victim=None,includeNone=True):
     
     if(numPlayable == 0): return None
-    # print(numPlayable, deck, deckhandlens)
     if(includeNone and not int(random.random()*(numPlayable+1))): return None
-    # if(includeNone and not random.randint(0,numPlayable)): return None
     
     if(victim == None): victim = 1 if int(name)==0 else 0
 
     possible = list(deck)
     possible[0] = 0
     possible[1] = 0
-    if deckhandlens[victim]:
+    if deckhandlens:
         possible[7] = possible[7]//2
         possible[8] = possible[8]//2
         possible[9] = possible[9]//2
@@ -204,8 +85,4 @@ def giveRandomMove(deck,name,deckhandlens,numPlayable,victim=None,includeNone=Tr
     if(possible==[0]*12): return None
     numbers = [0,1,2,3,4,5,6,7,8,9,10,11]
 
-    # return weighted_random_choice(numbers, possible)
     return random.choices(numbers, weights=possible, k=1)[0]
-
-def LCG(x, a, c, m): #https://www.ams.org/journals/mcom/1999-68-225/S0025-5718-99-00996-5/S0025-5718-99-00996-5.pdf
-    return (a*x+c)%m
