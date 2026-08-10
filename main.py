75057be9b2363fa2aa96a2e81ae0496ea6cd8664
FluffyCube9343 <learningninja007@gmail.com>
2026-08-10 18:47:48 -0400

    most of player

--- main.py
+++ main.py
@@ -9,8 +9,6 @@ class PublicKnowledge:
         self.deckSize = -1 #the game has not started yet
         self.playerSizes = [-1]*2 #the game has not started yet
         self.deckEpoch = 0 #incremented any time a shuffle is played or a EK is drawn to invalidate a STF
-
-
 # Currently just sitting here so STF actually gets implemeneted properly
 class STFKnowledge:
     def __init__(self, cards, deckSize, deckEpoch):
@@ -20,13 +18,59 @@ class STFKnowledge:
 
 class GameState:
     def __init__(self):
-        self.hands = [[0]*12,[0]*12]
+        self.hands = [None,None]
         self.deck = []
         self.pk = PublicKnowledge
         self.pendingStack = []
         self.curPlayer = 0
         self.turnsLeft = 1
         self.STFKnowledge = [None,None]
+
+class Action:
+    def __init__(self, card, target=-1):
+        self.card = card
+        self.target = target
+
+class Player:
+    def __init__(self, playerNum):
+        self.playerNum = playerNum
+    def chooseAction(self, state): #currently random action for base player
+        modified = list(state.hands[self.playerNum])
+
+        #cannot actually select a action if you can't do said action
+        modified[0] = 0
+        modified[1] = 1
+
+        #for now, you cannot favor or cat card the other player if they have no cards!
+        if(sum(state.hands[self.playerNum^1]) == 0):
+            modified[4] = 0
+            modified[7] = 0
+            modified[8] = 0
+            modified[9] = 0
+            modified[10] = 0
+            modified[11] = 0
+        else:
+            modified[7] = modified[7]//2
+            modified[8] = modified[8]//2
+            modified[9] = modified[9]//2
+            modified[10] = modified[10]//2
+            modified[11] = modified[11]//2
+
+        randaction = random.choices([*range(12)], weights = modified, k=1)
+        return randaction[0]
+
+    def doNope(self,state): #returns y/n if nope, contracts that it will deduct/be honest, currently just random
+        if(state.hands[self.playerNum][1] == 0):
+            return False
+        elif(random.random() < 0.5):
+            state.hands[self.playerNum][1] -= 1
+            return True
+        else:
+            return False
+
+    def gotFavored(self, state): #how else do you resolve a favor, you tell them which card you're giving away
+        pass
+
 
 def dealGame():
     pk = PublicKnowledge()
@@ -55,4 +99,6 @@ def run_game():
     pass
 
 def main():
-    pass
+    p1hand, p2hand, deck, pk = dealGame()
+    state = GameState()
+    state.hands = [p1hand, p2hand]
